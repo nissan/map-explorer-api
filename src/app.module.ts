a3e29405f65c6e72d15181ede494a8d6183a910b
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-07 02:36:44 +1000

    add the MapData module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MapDataModule } from './map-data/map-data.module';
 
 @Module({
-  imports: [],
+  imports: [MapDataModule],
   controllers: [AppController],
   providers: [AppService],
 })
