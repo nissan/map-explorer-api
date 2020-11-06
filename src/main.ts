25cb12c70a0759412bffb55a7d74f4b407dce802
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-07 03:10:26 +1000

    inject validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    forbidNonWhitelisted:true,
+    transform: true
+  }));
   const options = new DocumentBuilder()
     .setTitle('Map Explorer')
     .setDescription('The Map Explorer API')
