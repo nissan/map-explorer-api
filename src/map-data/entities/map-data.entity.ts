43bd853d1ed32e99a6ba37cea605b72da460f918
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-07 02:25:40 +1000

    adds map data entity based on csv layout

--- src/map-data/entities/map-data.entity.ts
+++ src/map-data/entities/map-data.entity.ts
@@ -1,1 +1,22 @@
-export class MapData {}
+export class MapData {
+    id: number;
+    vendorId: number;
+    pickupDateTime: Date;
+    dropoffDateTime:Date;
+    passengerCount: number;
+    tripDistance: number;
+    pickupLongitude: number;
+    pickupLatitude:number;
+    dropoffLongitude: number;
+    dropoffLatitude: number;
+    fareAmount:number;
+    tipAmount:number;
+    tollsAmount:number;
+    improvementSurchargeAmount:number;
+    totalAmount:number
+    extra:number;
+    mtaTax:number;
+    RateCodeId: number;
+    StoreAndForwardFlag:boolean;
+
+}
