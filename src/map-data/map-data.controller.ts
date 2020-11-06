f8956e917c024db67692132558efe8ffc02cd3d5
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-07 02:19:12 +1000

    injects MapDataService into the controller

--- src/map-data/map-data.controller.ts
+++ src/map-data/map-data.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { MapDataService } from './map-data.service';
 
 @Controller('map-data')
-export class MapDataController {}
+export class MapDataController {
+    constructor(private mapDataService: MapDataService){}
+}
