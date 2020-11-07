f0f4e7f884276305e6915ac21778b1013efa9619
Nissan Dookeran <nissan.dookeran@gmail.com>
2020-11-07 12:38:48 +1000

    use mapped-types to reuse DTO validation in Create with Update

--- src/taxi-trip/dtos/update-taxi-trip.dto.ts
+++ src/taxi-trip/dtos/update-taxi-trip.dto.ts
@@ -1,1 +1,4 @@
-export class UpdateTaxiTripDto {}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateTaxiTripDto } from "./create-taxi-trip.dto";
+
+export class UpdateTaxiTripDto extends PartialType(CreateTaxiTripDto) {}
